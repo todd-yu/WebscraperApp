f599bc99d1846689d1b114312423cd8d3b046c7e
Todd Yu <toddyu@berkeley.edu>
2019-11-21 23:24:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import webbrowser
 app = Flask(__name__)
 CORS(app)
 
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-soup = BeautifulSoup(page.text, 'html.parser')
+# page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+# soup = BeautifulSoup(page.text, 'html.parser')
 
+@app.route('/main', methods = ['GET', 'POST'])
 
-
-
-
+#sample comment for git push
 
 
 

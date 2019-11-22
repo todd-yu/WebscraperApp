739d893ccc00b017bad73d5661fe055516c6f555
Todd Yu <toddyu@berkeley.edu>
2019-11-21 22:56:42 -0800

    dd
    dd

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request, CORS
+import requests
+from bs4 import BeautifulSoup
+import webbrowser
+
+app = Flask(__name__)
+CORS(app)
+
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+soup = BeautifulSoup(page.text, 'html.parser')
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	app.run(debug = True)

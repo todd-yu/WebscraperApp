7f48809dbc2a699116e435aedcf9c624ce8dc02d
Todd Yu <toddyu@berkeley.edu>
2019-12-06 18:10:47 -0800

    added decoraters

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
-from flask import Flask, jsonify, request, CORS
+from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
 
 app = Flask(__name__)
-CORS(app)
 
 # page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
 # soup = BeautifulSoup(page.text, 'html.parser')
 
+
 @app.route('/main', methods = ['GET', 'POST'])
+def main():
+	raw_html = request.args.get('html') or None
+	"""main algorithm, takes in html and returns http reqeust to BetterNews in json format"""
+	# news_articles = 
+	# google_cloud()
+	# better_news()
+	
 
-#Shivam testing git
+	return jsonify(news_articles)
 
 
+@app.route('/test', methods = ['GET'])
+def http_test():
+	state1 = request.args.get('state') or None
+
+	if state1 == 1:
+
+
+		data = {
+
+		'todd':'master',
+		'shivam':'slave',
+		'naasir':'slave'
+
+		}
+
+		return jsonify(data) 
+	else:
+		return "failure"
 
 
 

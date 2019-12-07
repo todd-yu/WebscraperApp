685852bbc66a834fa844c481325b94b1b992a813
yodashiv <yodashiv@gmail.com>
2019-12-06 19:47:24 -0800

    Added git_ingore file, and a file to store the API key. Also added file for NLP logic

---
+++ NLP_keywords.py
@@ -0,0 +1,1 @@
+"""This module contains the logic to extract the keywords from a set of text."""
--- html_extracter.py
+++ html_extracter.py
@@ -9,15 +9,15 @@ page = requests.get("https://techcrunch.com/2019/12/06/elon-musk-found-not-liabl
 
 
 def extract_text(raw_html):
-    #Extracts all relevant text from article
+    """Extracts all relevant text from article"""
     soup = BeautifulSoup(raw_html)
 
     def get_title_data(html_soup):
-        #Returns the title of the article from HTML_SOUP
+        """Returns the title of the article from HTML_SOUP"""
         return str(soup.title.string)
 
     def get_paragraph_data(html_soup):
-        #Returns the paragraph text of the article from HTML_SOUP
+        """Returns the paragraph text of the article from HTML_SOUP"""
         polluted_text = str(soup.find_all("p"))
         text_soup = BeautifulSoup(polluted_text)
         return text_soup.get_text()
